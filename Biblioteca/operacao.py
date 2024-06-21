b46df1af423d0411b542aec19526c5a0881cb0ad
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-20 21:05:03 -0400

    .

--- Biblioteca/operacao.py
+++ Biblioteca/operacao.py
@@ -3,5 +3,6 @@ from .livro import Livro
 #Contém a classe Operacao.
 
 class Operacao:
-    def __init__(self):
-        pass
+    def __init__(self, tipo, livro: Livro, data_dia):
+        self.tipo = tipo
+        self.data = data_dia
