6d475e52baeeb7aa4c51c9974483ebfeb81469ae
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-20 21:08:15 -0400

    .

--- Biblioteca/livro.py
+++ Biblioteca/livro.py
@@ -1,7 +1,7 @@
 #Contém a classe Livro.
 
 class Livro:
-    def __init__(self, titulo, autor, ano_pub):
+    def __init__(self, titulo: str, autor: str, ano_pub: int):
         self.titulo = titulo
         self.autor = autor
         self.ano_pub = ano_pub
