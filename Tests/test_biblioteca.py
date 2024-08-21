af0a1436fc8a73ea8f4b96d48eeedfca5e118bc1
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-08-21 16:11:24 -0400

    .

--- Tests/test_biblioteca.py
+++ Tests/test_biblioteca.py
@@ -1,1 +1,24 @@
 #Testes para a classe Biblioteca.
+
+# test_biblioteca.py
+
+import unittest
+from biblioteca import Biblioteca
+from livro import Livro
+
+class TestBiblioteca(unittest.TestCase):
+    def test_adicionar_livro(self):
+        bib = Biblioteca()
+        livro = Livro("Título", "Autor", 2020, "1234567890")
+        bib.adicionar_livro(livro)
+        self.assertEqual(len(bib.livros), 1)
+
+    def test_remover_livro(self):
+        bib = Biblioteca()
+        livro = Livro("Título", "Autor", 2020, "1234567890")
+        bib.adicionar_livro(livro)
+        bib.remover_livro("1234567890")
+        self.assertEqual(len(bib.livros), 0)
+
+if __name__ == '__main__':
+    unittest.main()
