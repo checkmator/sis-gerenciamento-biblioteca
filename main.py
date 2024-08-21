33fed053f4f7ae49eaeeaa20182b48633d32cfbe
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-08-21 16:14:14 -0400

    .

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-#O ponto de entrada do seu programa. Aqui você pode instanciar a classe Biblioteca e implementar a lógica principal do seu programa.
+# main.py
+
 from biblioteca.biblioteca import Biblioteca
 from biblioteca.livro import Livro
 from datetime import datetime
 
-# Inicializa a biblioteca
 bib = Biblioteca()
 
-
-def menu():
-    print("Bem-vindo ao Sistema de Gerenciamento de Biblioteca!")
-    print("Por favor, selecione uma opção:")
-    print("1 - Adicionar Livro")
-    print("2 - Remover Livro")
-    print("3 - Buscar Livro")
-    print("4 - Listar Livros")
-    print("5 - Registrar Operação")
-    print("6 - Sair")
-
 def opcao1():
-    print("Você escolheu a Opção 1")
+    titulo = input("Digite o título do livro: ")
+    autor = input("Digite o autor do livro: ")
+    ano_pub = int(input("Digite o ano de publicação: "))
+    isbn = input("Digite o ISBN: ")
+    novo_livro = Livro(titulo, autor, ano_pub, isbn)
+    bib.adicionar_livro(novo_livro)
 
 def opcao2():
-    print("Você escolheu a Opção 2")
+    isbn = input("Digite o ISBN do livro a ser removido: ")
+    bib.remover_livro(isbn)
 
 def opcao3():
-    print("Você escolheu a Opção 3")
+    titulo = input("Digite o título do livro (ou deixe em branco): ")
+    autor = input("Digite o autor do livro (ou deixe em branco): ")
+    resultados = bib.buscar_livro(titulo, autor)
+    if resultados:
+        for livro in resultados:
+            print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_pub}, ISBN: {livro.isbn}")
+    else:
+        print("Nenhum livro encontrado.")
 
 def opcao4():
-    print("Você escolheu a Opção 4")
-
-def opcao5():
-    print("Você escolheu a Opção 4")
-
-def opcao6():
-    print("Você escolheu a Opção 6")
+    livros = bib.listar_livros()
+    for livro in livros:
+        print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_pub}, ISBN: {livro.isbn}")
 
 def main():
     while True:
